e6f8d84c8ef639cf906cccdf2d735af0cf4cdf24
dcodix <dcodina@laigu.net>
2012-09-06 17:05:31 +0200

    Added GPL.

--- mv-elsewhere.py
+++ mv-elsewhere.py
@@ -1,5 +1,14 @@
 #! /usr/bin/env python
 
+# Copyright (C) 2012 dcodix 
+
+# This file may be distributed and/or modified under the terms of
+# the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+# This file is distributed without any warranty; without even the implied
+# warranty of merchantability or fitness for a particular purpose.
+# See "LICENSE.GPL" in the source distribution for more information.
+
 """ This script will take filenames from the stdin and copy/move them to a
 directory destination preserving the directory tree and atributes.
 
